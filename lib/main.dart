98a2732c0bb0e8e2bf4511ee2b45effac49c602b
milordmariusz <komodo454@wp.pl>
2022-03-15 18:08:07 +0100

    Added readme and hide bars

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'home_screen.dart';
 
 void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((value) => runApp(const MCUAPI()));
   runApp(MCUAPI());
 }
 
@@ -12,6 +17,7 @@ class MCUAPI extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
